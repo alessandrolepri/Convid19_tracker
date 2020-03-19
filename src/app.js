5ba24bdd03b731bc5b172420357e3d577fad168a
alepri <alessandro.lepri@selfridges.co.uk>
2020-03-19 18:24:52 +0000

    Changed js

--- src/app.js
+++ src/app.js
@@ -3,21 +3,6 @@ import './scss/style.scss'
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-                "x-rapidapi-key": "9a234295f4msh7754bbf6c7eedd0p1a39d2jsnb6097b10e994"
-            }
-        })
-        .then(response => response.json()
-            .then(data => {
-                console.log(data);
-            }))
-        .catch(err => {
-            console.log(err);
-        });
-
     //Decalring the Different Variable and Objects
     let new_cases = document.getElementById("new_case");
     let new_death = document.getElementById("new_death");
@@ -37,15 +22,16 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .then(response => response.json()
             .then(data => {
-                console.log(data);
+                // console.log(data);
                 total_cases.innerHTML = data.total_cases;
                 new_cases.innerHTML = data.new_cases;
                 new_death.innerHTML = data.new_deaths;
                 total_death.innerHTML = data.total_deaths;
                 total_recovered.innerHTML = data.total_recovered;
 
-            })).catch(err => {
-            console.log(err);
+            }))
+        .catch(err => {
+            console.log('Looks like there was a problem: \n2', err);
         });
 
     //Fetching The Case by Country Data
@@ -58,11 +44,11 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .then(response => response.json()
             .then(data => {
-                console.log(data)
+                // console.log(data)
                 let countries_stat = data.countries_stat;
                 //Getting all the country statistic using a loop
                 for (let i = 0; i < countries_stat.length; i++) {
-                    console.log(countries_stat[i]);
+                    // console.log(countries_stat[i]);
                     //we will start by inserting the new rows inside our table
                     let row = table.insertRow(i + 1);
                     let country_name = row.insertCell(0);
@@ -73,6 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     let recovered_per_country = row.insertCell(5);
                     let active_cases = row.insertCell(6) //
                     let serious_critical = row.insertCell(7);
+                    let case_population = row.insertCell(8);
 
                     country_name.innerHTML = countries_stat[i].country_name;
                     total_cases.innerHTML = countries_stat[i].cases;
@@ -82,10 +69,11 @@ document.addEventListener("DOMContentLoaded", () => {
                     recovered_per_country.innerHTML = countries_stat[i].total_recovered;
                     active_cases.innerHTML = countries_stat[i].active_cases;
                     serious_critical.innerHTML = countries_stat[i].serious_critical;
+                    case_population.innerHTML = countries_stat[i].total_cases_per_1m_population;
 
                 }
             }))
         .catch(err => {
-            console.log(err);
+            console.log('Looks like there was a problem: \n3', err);
         });
 })
